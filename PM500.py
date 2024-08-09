4581c0618a3cebeaeb96cd8e13eab21d1176a340
qzertywsx <qzertywsx@users.noreply.github.com>
2024-08-09 15:44:38 +0200

    Create PM500.py

---
+++ PM500.py
@@ -0,0 +1,189 @@
+import pymodbus
+from pymodbus.pdu import ModbusRequest
+from pymodbus.client import ModbusSerialClient as ModbusClient
+from pymodbus.transaction import ModbusRtuFramer
+import logging
+
+logging.basicConfig()
+log = logging.getLogger()
+#log.setLevel(logging.DEBUG)
+log.setLevel(logging.ERROR)
+
+class PM500:
+	def __init__(self, port, baudrate, unit_id, currentScale=1.0):
+		self.port = port
+		self.baudrate = baudrate
+		self.unit_id = unit_id
+		self.currentScale = currentScale
+		self.client = ModbusClient(port=self.port, baudrate=self.baudrate)
+		print(self.client)
+
+	def __str__(self):
+		return "PM500 port: " + self.port + ", baudrate: " + str(self.baudrate) + ", unit_id: " + str(self.unit_id) + ", Connected: " + str(self.client.connected)
+
+	def check_connection(self):
+		try:
+			self.client.connect()
+			return self.client.connected
+		except:
+			return False
+	
+	def disconnect(self):
+		self.client.close()
+
+	def __getint32(self,data):
+		try:
+			return ModbusClient.convert_from_registers(data.registers, ModbusClient.DATATYPE.INT32)
+		except:
+			return False
+	
+	def __dataToVoltage(self,data):
+		return self.__getint32(data)/100
+	
+	def __dataToCurrent(self,data):
+		return self.__getint32(data)/1000
+	
+	def __dataToCosfi(self,data):
+		return self.__getint32(data)/1000
+	
+	def __dataToPower(self,data):
+		return self.__getint32(data)/100
+	
+	def __dataToEnergy(self,data):
+		return self.__getint32(data)
+	
+	def __dataToFrequency(self,data):
+		return self.__getint32(data)/100
+		
+	def __dataToHarminic(self,data):
+		return data.registers[0]/1000
+		
+	def __dataToHour(self,data):
+		return self.__getint32(data)/100
+	
+	def getSinglePhase(self):
+		try:
+			U     = self.__dataToVoltage(self.client.read_holding_registers(0x030e, 2))
+			I     = self.__dataToCurrent(self.client.read_holding_registers(0x0300, 2))*self.currentScale
+			P     = self.__dataToPower(  self.client.read_holding_registers(0x031e, 2))*self.currentScale
+			Q     = self.__dataToPower(  self.client.read_holding_registers(0x0324, 2))*self.currentScale
+			S     = self.__dataToPower(  self.client.read_holding_registers(0x032a, 2))*self.currentScale
+			Cosfi = self.__dataToCosfi(  self.client.read_holding_registers(0x0368, 2))
+			return {"U": U, "I": I, "P": P, "Q": Q, "S": S, "Cosfi": Cosfi}
+		except Exception as e:
+			return False
+	
+	def getVoltage(self):
+		try:
+			U12 = self.__dataToVoltage(self.client.read_holding_registers(0x0308, 2))
+			U23 = self.__dataToVoltage(self.client.read_holding_registers(0x030a, 2))
+			U31 = self.__dataToVoltage(self.client.read_holding_registers(0x030c, 2))
+			U1  = self.__dataToVoltage(self.client.read_holding_registers(0x030e, 2))
+			U2  = self.__dataToVoltage(self.client.read_holding_registers(0x0310, 2))
+			U3  = self.__dataToVoltage(self.client.read_holding_registers(0x0312, 2))
+			return {"U12": U12, "U23": U23, "U31": U31, "U1": U1, "U2": U2, "U3": U3}
+		except Exception as e:
+			return False
+	
+	def getCurrent(self):
+		try:
+			I1 = self.__dataToCurrent(self.client.read_holding_registers(0x0300, 2))
+			I2 = self.__dataToCurrent(self.client.read_holding_registers(0x0302, 2))
+			I3 = self.__dataToCurrent(self.client.read_holding_registers(0x0304, 2))
+			In = self.__dataToCurrent(self.client.read_holding_registers(0x0306, 2))
+			return {"I1": I1, "I2": I2, "I3": I3, "In": In}
+		except Exception as e:
+			return False
+	
+	def getTotalPower(self):
+		try:
+			P = self.__dataToPower(self.client.read_holding_registers(0x0316, 2))
+			Q = self.__dataToPower(self.client.read_holding_registers(0x0318, 2))
+			S = self.__dataToPower(self.client.read_holding_registers(0x031a, 2))
+			return {"P": P, "Q": Q, "S": S}
+		except Exception as e:
+			return False
+	
+	def getActivePower(self):
+		try:
+			P1 = self.__dataToPower(self.client.read_holding_registers(0x031e, 2))
+			P2 = self.__dataToPower(self.client.read_holding_registers(0x0320, 2))
+			P3 = self.__dataToPower(self.client.read_holding_registers(0x0322, 2))
+			return {"P1": P1, "P2": P2, "P3": P3}
+		except Exception as e:
+			return False
+	
+	def getReactivePower(self):
+		try:
+			Q1 = self.__dataToPower(self.client.read_holding_registers(0x0324, 2))
+			Q2 = self.__dataToPower(self.client.read_holding_registers(0x0326, 2))
+			Q3 = self.__dataToPower(self.client.read_holding_registers(0x0328, 2))
+			return {"Q1": Q1, "Q2": Q2, "Q3": Q3}
+		except Exception as e:
+			return False
+	
+	def getApparentPower(self):
+		try:
+			S1 = self.__dataToPower(self.client.read_holding_registers(0x032a, 2))
+			S2 = self.__dataToPower(self.client.read_holding_registers(0x032c, 2))
+			S3 = self.__dataToPower(self.client.read_holding_registers(0x032e, 2))
+			return {"S1": S1, "S2": S2, "S3": S3}
+		except Exception as e:
+			return False
+	
+	def getCosfi(self):
+		try:
+			Cosfi_Tot = self.__dataToCosfi(self.client.read_holding_registers(0x0366, 2))
+			Cosfi_1   = self.__dataToCosfi(self.client.read_holding_registers(0x0368, 2))
+			Cosfi_2   = self.__dataToCosfi(self.client.read_holding_registers(0x036a, 2))
+			Cosfi_3   = self.__dataToCosfi(self.client.read_holding_registers(0x036c, 2))
+			return {"Cosfi tot": Cosfi_Tot, "Cosfi 1": Cosfi_1, "Cosfi 2": Cosfi_2, "Cosfi 3": Cosfi_3}
+		except Exception as e:
+			return False
+	
+	def getEnergy(self):
+		try:
+			E_Active        = self.__dataToEnergy(self.client.read_holding_registers(0x0358, 2))
+			E_Reactive      = self.__dataToEnergy(self.client.read_holding_registers(0x035a, 2))
+			E_Apparent      = self.__dataToEnergy(self.client.read_holding_registers(0x035c, 2))
+			E_ActiveMinus   = self.__dataToEnergy(self.client.read_holding_registers(0x035e, 2))
+			E_ReactiveMinus = self.__dataToEnergy(self.client.read_holding_registers(0x0360, 2))
+			return {"E active": E_Active, "E reactive": E_Reactive, "E apparent": E_Apparent, "E active-": E_ActiveMinus, "E reactive-": E_ReactiveMinus}
+		except Exception as e:
+			return False
+	
+	def getHour(self):
+		try:
+			return self.__dataToHour(self.client.read_holding_registers(0x0356, 2))
+		except Exception as e:
+			return False
+	
+	def getFrequency(self):
+		try:
+			return self.__dataToFrequency(self.client.read_holding_registers(0x0314, 2))
+		except Exception as e:
+			return False
+
+	def getTHD(self):
+		try:
+			THD_U12 = self.__dataToHarminic(self.client.read_holding_registers(0x0904, 1))
+			THD_U23 = self.__dataToHarminic(self.client.read_holding_registers(0x0905, 1))
+			THD_U31 = self.__dataToHarminic(self.client.read_holding_registers(0x0906, 1))
+			THD_U1  = self.__dataToHarminic(self.client.read_holding_registers(0x0907, 1))
+			THD_U2  = self.__dataToHarminic(self.client.read_holding_registers(0x0908, 1))
+			THD_U3  = self.__dataToHarminic(self.client.read_holding_registers(0x0909, 1))
+			THD_I1  = self.__dataToHarminic(self.client.read_holding_registers(0x0900, 1))
+			THD_I2  = self.__dataToHarminic(self.client.read_holding_registers(0x0901, 1))
+			THD_I3  = self.__dataToHarminic(self.client.read_holding_registers(0x0902, 1))
+			THD_In  = self.__dataToHarminic(self.client.read_holding_registers(0x0903, 1))
+			return {"THD U12": THD_U12, "THD U23": THD_U23, "THD U31": THD_U31, "THD U1": THD_U1, "THD U2": THD_U2, "THD U3": THD_U3, "THD I1": THD_I1, "THD I2": THD_I2, "THD I3": THD_I3, "THD In": THD_In}
+		except Exception as e:
+			return False
+	
+	def getSinglePhaseTHD(self):
+		try:
+			THD_U = self.__dataToHarminic(self.client.read_holding_registers(0x0907, 1))
+			THD_I = self.__dataToHarminic(self.client.read_holding_registers(0x0900, 1))
+			return {"THD U": THD_U, "THD I": THD_I}
+		except Exception as e:
+			return False
